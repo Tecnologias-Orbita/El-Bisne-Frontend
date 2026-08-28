0a6e07e3091d04e27a8a59b2761c620a6517a86e
Lazaro <lachy200408@gmail.com>
2026-08-28 14:15:12 -0500

    feat(seo): Adding basic SEO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
 import { PlatformHomePage } from "@/modules/platform-public/pages/PlatformHomePage";
+import { seo } from "@/lib/seo/seo";
+
+export const metadata = seo("Inicio");
 
 export default function Page() {
   return <PlatformHomePage />;
